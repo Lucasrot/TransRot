5a578ae3861ed8fa61bb699ca48460afbc1bf228
Lucas Roteño <lucasrot1@hotmail.com>
2024-08-15 15:39:16 -0300

    landing terminada

---
+++ src/util/countdown.js
@@ -0,0 +1,11 @@
+export function getTimeRemaining(endtime) {
+    const now = new Date();
+    const timeRemaining = endtime - now;
+  
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  
+    return { days, hours, minutes, seconds };
+  }
